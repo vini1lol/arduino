821d0c75e302d80cc185c134764b68db7eadcc2c
LSodre <37416482+LSodre@users.noreply.github.com>
2019-11-01 14:49:11 -0300

    Add files via upload

---
+++ mqtt_esp8266Recebe.ino
@@ -0,0 +1,67 @@
+#include "ESP8266WiFi.h";
+#include "PubSubClient.h";
+ 
+const char* ssid = "Leonardo";
+const char* password =  "12345678";
+const char* mqttServer = "mqtt.flespi.io";
+const int mqttPort = 1883;
+const char* mqttUser = "FlespiToken vd4VcscgPGf9iUvUSKPuXWOCJi0O7iStjMek4gqIKvHTe0DUAgcl3QOAWCdUKVdS";
+const char* mqttPassword = "12345678";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("test", "Hello from ESP8266");
+  client.subscribe("test");
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void loop() {
+  client.loop();
+}
